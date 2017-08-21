ca56795ecc7d097e0a8a54729fee7d1dab37bf76
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-08-21 13:42:13 +0200

    Add documentation

--- src/SpecificClassCollection/SpecificClassCollection.php
+++ src/SpecificClassCollection/SpecificClassCollection.php
@@ -10,17 +10,33 @@ abstract class SpecificClassCollection implements Countable, Iterator
     private $collection;
     private $current;
 
+    /**
+     * SpecificClassCollection constructor.
+     */
     public function __construct()
     {
         $this->collection = [];
         $this->current = 0;
     }
 
+    /**
+     * Get the number of elements inside
+     * the collection
+     *
+     * @return int
+     */
     public function count()
     {
         return count($this->collection);
     }
 
+    /**
+     * Add an element to the collection
+     *
+     * @param $element
+     * @return bool
+     * @throws InvalidClassException
+     */
     public function add($element)
     {
         $this->checkElementIsValid($element);
@@ -29,6 +45,14 @@ abstract class SpecificClassCollection implements Countable, Iterator
         return true;
     }
 
+    /**
+     * Check if an element can be added to
+     * the collection
+     *
+     * @param $element
+     * @return bool
+     * @throws InvalidClassException
+     */
     private function checkElementIsValid($element)
     {
         $elementClass = get_class($element);
@@ -40,39 +64,71 @@ abstract class SpecificClassCollection implements Countable, Iterator
         return true;
     }
 
+    /**
+     * Get the FQDN of the class we
+     * want to store in the collection
+     *
+     * @return string
+     */
     abstract protected function getValidClassName();
 
+    /**
+     * Empty the collection
+     */
     public function clear()
     {
         $this->collection = [];
         $this->rewind();
     }
 
+    /**
+     * Return the array of elements
+     *
+     * @return array
+     */
     public function getElements()
     {
         return $this->collection;
     }
 
+    /**
+     * @inheritdoc
+     * @return mixed
+     */
     public function current()
     {
         return $this->collection[$this->current];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function next()
     {
         ++$this->current;
     }
 
+    /**
+     * @inheritdoc
+     * @return int
+     */
     public function key()
     {
         return $this->current;
     }
 
+    /**
+     * @inheritdoc
+     * @return bool
+     */
     public function valid()
     {
         return isset($this->collection[$this->current]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function rewind()
     {
         $this->current = 0;
